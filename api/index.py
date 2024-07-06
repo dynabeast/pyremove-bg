3b5102b6eadc3fd3e94a8906785e37e9d0821d82
unknown <dbdynabeast@gmail.com>
2024-07-07 02:02:09 +0530

    done

--- api/index.py
+++ api/index.py
@@ -1,27 +1,15 @@
-from flask import Flask, request, send_file,jsonify
-from rembg import remove
-from PIL import Image
-import io
+# /api/index.py
+
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/remove-bg', methods=['POST'])
-def remove_bg():
-    if 'image' not in request.files:
-        return "No image file provided", 400
 
-    file = request.files['image']
-    img = Image.open(file)
-    img_no_bg = remove(img)
+@app.route("/")
+def home():
+    return "Flask Vercel Example - Hello World", 200
 
-    img_byte_arr = io.BytesIO()
-    img_no_bg.save(img_byte_arr, format='PNG')
-    img_byte_arr.seek(0)
 
-    return send_file(img_byte_arr, mimetype='image/png')
-
-@app.route('/health', methods=['GET'])
-def health_check():
-    return jsonify(status="Server is running"), 200
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+@app.errorhandler(404)
+def page_not_found(e):
+    return jsonify({"status": 404, "message": "Not Found"}), 404
