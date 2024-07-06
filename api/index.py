19aa7df60a05a0d5f7786ebdbf6ee506ad5b5772
unknown <dbdynabeast@gmail.com>
2024-07-07 00:40:20 +0530

    don

--- api/index.py
+++ api/index.py
@@ -1,6 +1,12 @@
-from flask import Flask, jsonify
+from flask import Flask, send_from_directory
+import os
 
 app = Flask(__name__)
+
+# Serve favicon.ico from the static directory
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 @app.route("/")
 def home():
